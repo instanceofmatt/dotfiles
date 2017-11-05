71ea72d0d44dc9a01c1850868b88daf72ebc1ebb
Matt Burgess <matt.burgess92@googlemail.com>
2017-11-05 20:25:46 +0000

    Added awesome alias/function created by @imjoshholloway

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,10 @@ alias weather="curl -4 http://wttr.in"
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias nah="git reset --hard"
 alias reload="source $HOME/.zshrc"
+alias aws_whoami="aws sts get-caller-identity"
+
+function switch_profile() {
+    export AWS_PROFILE="${1}"
+    export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id --profile ${1})
+    export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key --profile ${1})
+}
